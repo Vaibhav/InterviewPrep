64a49aa346a7767de63cf061bdd94ae19001d5e9
Vaibhav <ishuvaibhav@gmail.com>
2016-06-17 16:48:09 -0400

    Problems 4, 5, 6, 7

---
+++ test.py
@@ -0,0 +1,41 @@
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+max_pal = 0
+
+for i in range(999, 99, -1):
+    for j in range(999, 99, -1):
+		pal = i * j;
+		if(is_palindrome(pal)) and pal > max_pal:
+			max_pal = pal
+
+
+print "Largest palindrome which is created by multiplying two 3-digit numbers: " + str(max_pal);
+
+#906609
+
+#====================================
+
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+#worked out prime facotrization
+num = 1*2*3*5*7*11*13*17*19*2*2*3*2
+print "LCM" + str(num);
+
+
+# used brute force to check for LCM(1, range(1,21))
+# cut down some conditionals by eliminating thm if two other factors were multiplied to create them.
+x = 10000
+while(1):
+    if ((x % 5 == 0) and (x % 7 == 0) and (x % 9 == 0) and (x % 11 == 0) and (x % 12 == 0)
+        and (x % 13 == 0) and (x % 16 == 0) and (x % 17 == 0) and (x % 19 == 0)):
+        print "LCM of all the numbers between 1 and 20: " + str(x)
+        break;
+    else:
+        # has to be even so able to increment by 2
+        # obviously a large number so starting at higher number than given (2520)
+        x = x + 2
+
+#232792560
