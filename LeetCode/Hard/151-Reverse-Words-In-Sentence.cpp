b1026704052d283a12ac23096e6a56a09692e1e2
Vaibhav Khaitan <ishuvaibhav@gmail.com>
2017-10-13 21:44:59 -0400

    Create 151-Reverse-Words-In-Sentence.cpp

---
+++ LeetCode/Hard/151-Reverse-Words-In-Sentence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+        if(s.empty()) return;
+        
+        int len1 = s.size();
+        int l=0;
+        
+        for(int i = 0; i < len; i++) {
+            if(s[i] != ' ' || (i > 0 && s[i-1] != ' '))
+                s[l++] = s[i];
+        }
+        
+        if (l > 0 && s[l-1] == ' ') l--;
+        s = s.substr(0,l);
+        if (s.empty()) return;
+        
+        reverse(s.begin(), s.end());
+        
+        l = 0;
+        
+        for(int i = 0;i < s.size(); i++) {
+            
+            if(s[i] == ' ') {
+                reverse(s.begin()+l, s.begin()+i);
+                l = i+1;
+            }
+        }
+        
+        reverse(s.begin()+l, s.end());
+    }
+};
